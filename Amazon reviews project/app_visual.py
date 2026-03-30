1417596edb96bc400f872797c429f36416c2f663
dirigaca11-web <dirigaca11@gmail.com>
2026-03-30 10:46:24 -0600

    Update app_visual.py

--- Amazon reviews project/app_visual.py
+++ Amazon reviews project/app_visual.py
@@ -1,12 +1,16 @@
 
 import streamlit as st
 import joblib
+import os
 
 st.title("Analizador de Sentimientos - Amazon Reviews")
 review = st.text_area("Escribe tu reseña aquí:")
+base_path = os.path.dirname(__file__)
 
-model = joblib.load('modelo_amazon.pkl')
-vectorizer = joblib.load('vectorizador.pkl')
+model_path = os.path.join(base_path, 'modelo_amazon.pkl')
+vect_path = os.path.join(base_path, 'vectorizador.pkl')
+model = joblib.load(model_path)
+vectorizer = joblib.load(vect_path)
 
 if st.button("Predecir"):
     vector = vectorizer.transform([review.lower()])
